6e289d923c462a6de39f602e64d4df72888dd3a8
daora006 <severin.mboukou@gmail.com>
2020-03-04 21:14:58 +0100

    Stack resized, ZStack added, buttons updated, new image added in the main view.

--- Tenla/View/ContentView.swift
+++ Tenla/View/ContentView.swift
@@ -18,48 +18,64 @@ struct ContentView: View {
         
         NavigationView {
             
-            VStack{
+            ZStack {
                 
-                
-                Color("main yellow")
+                Color("main red")
                     .edgesIgnoringSafeArea(.all)
                 
-                VStack(alignment: .leading) {
-                    Text("Menu principal")
-                        .fontWeight(.regular)
-                    Text("")
-                        .font(.subheadline)
-                    Divider()
-                    NavigationLink(destination: ChannelView()){
-                        Text("Ajouter une chaîne")
+                VStack {
+                    VStack(alignment: .center) {
+                        Image("lemon")
+                            .resizable()
+                            .frame(width:210, height:210, alignment: .center)
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle().stroke(Color.gray, lineWidth: 0.1))
+                    }.frame(height: 150)
+                    //                    Spacer()
+                    
+                    //                        .padding(.bottom)
+                    //Spacer()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Menu principal")
+                            .fontWeight(.regular)
+                        Text("")
+                            .font(.subheadline)
+                        Divider()
+                        NavigationLink(destination: ChannelView()){
+                            Text("Ajouter une chaîne").foregroundColor(Color("main yellow"))
+                        }
+                        Button(action: {
+                            self.showingSecondVC.toggle()
+                        }){
+                            Text("À Propos de Tenla").foregroundColor(Color("main yellow"))
+                        }
+                        .sheet(isPresented: $showingSecondVC){
+                            SecondView()
+                        }
                     }
-                    Button(action: {
-                        self.showingSecondVC.toggle()
-                    }){
-                        Text("À Propos de Tenla")
-                    }.sheet(isPresented: $showingSecondVC){
-                        SecondView()
-                    }
-                    Spacer()
+                    .frame(height: 300)
+                    
+                    //Spacer()
                 }.padding()
-                Color("main yellow")
+                    // Color("main yellow")
                     .edgesIgnoringSafeArea(.bottom)
                 
                 
-                
             }.navigationBarTitle("Tenla")
-                .background(Color("main yellow"))
+            //.background(Color("main yellow"))
         }
         
     }
     
 }
-    
-    
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
